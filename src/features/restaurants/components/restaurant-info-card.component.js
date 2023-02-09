ab11b90a873491cdced4ca69d45cadd7abfa6b80
Jerricke <eejerrickw@gmail.com>
2023-02-08 17:31:15 -0800

    Added spacers

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,33 +1,97 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "../components/spacer/spacer.components";
 
 const Title = styled.Text`
-  padding: 16px;
-  color: red;
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
 `;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const RestaurantCard = styled(Card)`
+  color: ${(props) => props.theme.colors.ui.tertiary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.ui.tertiary};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)``;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://cdn.vox-cdn.com/thumbor/aXnSLwLY9ClmVkx8fWcM6709OoM=/1400x788/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23182932/shutterstock_1538500832.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Title> {name} </Title>
-    </Card>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title> {name} </Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                Closed Temporarily
+              </Text>
+            )}
+            <Spacer position="left" size="small">
+              {isOpenNow && <Open xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 16, height: 16 }} source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address> {address} </Address>
+      </Info>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,33 +1,66 @@
+// import React from "react";
+// import { Searchbar } from "react-native-paper";
+// import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+// import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+// import styled from "styled-components/native";
+
+// const SearchContainer = styled.View`
+//   padding: ${(props) => props.theme.space[3]};
+//   background-color: ${(props) => props.theme.colors.brand.primary};
+// `;
+
+// const SafeArea = styled(SafeAreaView)`
+//   flex: 1;
+//   ${StatusBar.currentheight && `margin-top: ${StatusBar.currentheight}px`}; sdf
+// `;
+// // && says that if "statusbar.currenheight" has a value, then execute _____
+
+// const ListBox = styled.View`
+//   flex: 1;
+//   padding: ${(props) => props.theme.space[3]};
+//   background-color: ${(props) => props.theme.colors.brand.secondary};
+// `;
+// export const RestaurantsScreen = () => {
+//   return (
+//     <SafeArea>
+//       <SearchContainer>
+//         <Searchbar />
+//       </SearchContainer>
+//       <ListBox>
+//         <RestaurantInfoCard />
+//       </ListBox>
+//     </SafeArea>
+//   );
+// };
+
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import styled from "styled-components/native";
+
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
-export const RestaurantsScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.searchBox}>
-        <Searchbar placeholder="Search" />
-      </View>
-      <View style={styles.listBox}>
-        <RestaurantInfoCard />
-      </View>
-    </SafeAreaView>
-  );
-};
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  searchBox: {
-    padding: 16,
-    backgroundColor: "powderblue",
-  },
-  listBox: {
-    flex: 1,
-    padding: 16,
-    // backgroundColor: "blue",
-  },
-});
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
+);
